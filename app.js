fa212fbe059e90bd82ac0616d5b9b58e05c30062
Tobi Banks <tobibanks@yahoo.co.uk>
2021-01-28 23:45:49 +0000

    restyled elements. Started JS logic

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+// Book constructor
+function Book(title, author, status) {
+	this.title = title;
+	this.author = author;
+	this.status = status;
+	this.info = function () {
+		return `${title}, ${author}, ${status}`;
+	};
+}
+
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
-}
-
+// 
 function addBookToLibrary() {
-  // do stuff here
-}
+  const bookVal = document.querySelector('.style-input');
+  const authVal = document.querySelector('.style-author');
+  const readStatus = document.querySelector('.status');
+} 
+
+console.log(addBookToLibrary());
+/*
+const results = document.querySelector('.dom-results');
+const div = document.createElement('div');
+*/
