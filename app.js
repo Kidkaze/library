19dc9f4605c21c6d8fae6ab0e565b143fc9dcc68
Tobi Banks <tobibanks@yahoo.co.uk>
2021-02-04 22:29:17 +0000

    updated adding book objects to library. Working on appending to page

--- app.js
+++ app.js
@@ -3,23 +3,32 @@ function Book(title, author, status) {
 	this.title = title;
 	this.author = author;
 	this.status = status;
-	this.info = function () {
-		return `${title}, ${author}, ${status}`;
-	};
 }
 
 // Variables
+let myLibrary = [];
 const bookVal = document.querySelector('.style-input');
 const authVal = document.querySelector('.style-author');
-const readStatus = document.querySelector('.status');
+const readStatus = document.querySelector('select');
 const button = document.querySelector('.button');
-let myLibrary = [];
+const results = document.querySelector('.dom-results');
 
-// add books to library array
+// Add book objects to library
 function addBookToLibrary() {
-	let userInput =	button.addEventListener('click', () => {
-		console.log(`${bookVal.value} ${authVal.value}`);
+	button.addEventListener('click', () => {
+		const newBook = new Book(bookVal.value, authVal.value, readStatus.value); 
+		return myLibrary.push(newBook);
 	});
-} 
+}
 
 addBookToLibrary();
+
+/* Append books to webpage
+function displayBooks() {
+	for (i = 0; i <= myLibrary.length; i++) {
+		results = myLibrary.innerHTML;
+	}
+}
+
+displayBooks();
+*/
